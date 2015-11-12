831e6c250e56dfce48914d39acb8b8fc52d01117
Ben Johnson <ben@prettyswift.co>
2015-11-12 15:22:26 -0500

    Add empty class for CircularImageView

---
+++ SwiftCatalog/SwiftCatalog/View/CircularImageView.swift
@@ -0,0 +1,21 @@
+//
+//  CircularImageView.swift
+//  SwiftCatalog
+//
+//  Created by Ben Johnson on 11/12/15.
+//  Copyright © 2015 Pretty Swift. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class CircularImageView: UIImageView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
