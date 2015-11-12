bce464f9b14fcccd4802ea7063c41250064eba8b
Ben Johnson <ben@prettyswift.co>
2015-11-12 17:19:17 -0500

    Solution to views project

--- SwiftCatalog/SwiftCatalog/View/CircularImageView.swift
+++ SwiftCatalog/SwiftCatalog/View/CircularImageView.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 @IBDesignable class CircularImageView: UIImageView {
+    
+    lazy var borderLayer: CAShapeLayer = {
+       let shapeLayer = CAShapeLayer()
+        shapeLayer.lineWidth = 2.0
+        shapeLayer.strokeColor = self.tintColor.CGColor
+        shapeLayer.fillColor = UIColor.clearColor().CGColor
+        self.layer.addSublayer(shapeLayer)
+        return shapeLayer
+    }()
 
-    // TODO: Change this file to make the image view circular and have a customizable border color
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let path = UIBezierPath(ovalInRect: bounds)
+        
+        borderLayer.path = path.CGPath
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.CGPath
+        layer.mask = shapeLayer
+    }
+    
+    override func tintColorDidChange() {
+        borderLayer.strokeColor = tintColor.CGColor
+    }
 
 }
